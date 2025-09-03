f64aedfe2316ea85cccf1e0788bf47b971d03bce
YuNanSong <1210401929@q.com>
2025-09-03 19:09:42 +0800

    commit code

--- ai_api/src/main/java/com/example/ai_api/AiApiApplication.java
+++ ai_api/src/main/java/com/example/ai_api/AiApiApplication.java
@@ -3,10 +3,13 @@ package com.example.ai_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication(scanBasePackages = "com.example")
 //声明为注册服务
 @EnableEurekaClient
+//使用redis存储session
+@EnableRedisHttpSession
 public class AiApiApplication {
 
     public static void main(String[] args) {
--- ai_api/src/main/java/com/example/ai_api/controller/AiController.java
+++ ai_api/src/main/java/com/example/ai_api/controller/AiController.java
@@ -4,11 +4,14 @@ import com.alibaba.dashscope.exception.InputRequiredException;
 import com.alibaba.dashscope.exception.NoApiKeyException;
 import com.example.common_api.bean.ResultBody;
 import com.example.ai_api.service.AlibabaAiService;
+import com.example.common_api.bean.UserBean;
 import com.example.common_api.util.AesUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 
 import javax.servlet.http.HttpSession;
@@ -31,7 +34,11 @@ public class AiController {
     //流式输出  前端也必须流式接收
     @RequestMapping(value = "/qWenStream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     @ResponseBody
-    public Flux<String> qWenStream(@RequestParam String message) throws NoApiKeyException, InputRequiredException {
+    public Flux<String> qWenStream(@RequestParam String message,HttpSession session) throws NoApiKeyException, InputRequiredException {
+        UserBean userInfo = (UserBean)session.getAttribute("userInfo");
+        if(userInfo==null){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "用户未登录");
+        }
         // 规范化 message，去掉空格/换行，并把空格还原为 '+'
         String normalizedMessage = message.trim()
                 .replace(' ', '+')
